75aebc3cf46796d1357d3e4c2ced78a41bb4999f
Veysel Ozdemir <wishell.rock.4@gmail.com>
2024-01-26 21:06:51 +0300

    new projects added

---
+++ other-projects/java101-projects/HackerRankChallenges/HackerRankIfElse.java
@@ -0,0 +1,24 @@
+package HackerRankChallenges;
+
+import java.util.Scanner;
+
+public class HackerRankIfElse {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if (n >= 2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if (n >= 6 && n <= 20) {
+                System.out.println("Weird");
+            } else if (n > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
